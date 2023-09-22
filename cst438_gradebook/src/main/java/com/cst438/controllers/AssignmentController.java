5cb07d4526d6fdf098a5f92dde52c823c19a0a95
Tila Tran <artran@csumb.edu>
2023-09-21 17:21:23 -0700

    Fixed Methods for adding, modifying, and deleting assignments - Tests complete

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,12 +1,15 @@
 package com.cst438.controllers;
 
-import com.cst438.domain.*;
+import com.cst438.domain.Assignment;
+import com.cst438.domain.AssignmentDTO;
+import com.cst438.domain.AssignmentRepository;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import javax.persistence.EntityNotFoundException;
-import java.sql.Date;
 import java.util.List;
 
 @RestController
@@ -22,7 +25,7 @@ public class AssignmentController {
 	@GetMapping("/assignment")
 	public AssignmentDTO[] getAllAssignmentsForInstructor() {
 		// get all assignments for this instructor
-		String instructorEmail = "dwisneski@csumb.edu";  // user name (should be instructor's email) 
+		String instructorEmail = "dwisneski@csumb.edu";  // username (should be instructor's email)
 		List<Assignment> assignments = assignmentRepository.findByEmail(instructorEmail);
 		AssignmentDTO[] result = new AssignmentDTO[assignments.size()];
 		for (int i=0; i<assignments.size(); i++) {
@@ -39,47 +42,50 @@ public class AssignmentController {
 	}
 
 	@PostMapping("/assignment")
-	public ResponseEntity<String> addAssignment() {
+	public int addAssignment(@RequestBody AssignmentDTO adto){
+		String instructorEmail = "dwisneski@csumb.edu";
+
+		Course c = courseRepository.findById(adto.courseId()).orElse(null);
+		if(c == null || !c.getInstructor().equals(instructorEmail)){
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "course id not found");
+		}
+
 		Assignment assignment = new Assignment();
-		assignment.setName(assignment.getName());
-		assignment.setDueDate(Date.valueOf(assignment.getDueDate().toLocalDate()));
+		assignment.setCourse(c);
+		assignment.setDueDate(java.sql.Date.valueOf(adto.dueDate()));
+		assignment.setName(adto.assignmentName());
 
-		Course course = courseRepository.findById(assignment.getCourse().getCourse_id())
-				.orElseThrow(() -> new EntityNotFoundException("Course not found"));
-
-		assignment.setCourse(course);
-
+		// Add assignment
 		assignmentRepository.save(assignment);
 
-		return ResponseEntity.ok("Assignment added successfully");
+		return assignment.getId();
 	}
 
 	@PutMapping("/assignment/{assignmentId}")
-	public ResponseEntity<String> modifyAssignment(@PathVariable int assignmentId) {
-		Assignment assignment = assignmentRepository.findById(assignmentId)
-				.orElseThrow(() -> new EntityNotFoundException("Assignment not found"));
+	public void updateAssignment(@PathVariable int assignmentId, @RequestBody AssignmentDTO updatedAssignment) {
+		// Check if the assignment exists
+		Assignment assignment = assignmentRepository.findById(assignmentId).orElse(null);
+		if (assignment == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment not found");
+		}
 
-		assignment.setName(assignment.getName());
-		assignment.setDueDate(Date.valueOf(assignment.getDueDate().toLocalDate()));
-
-		Course course = courseRepository.findById(assignment.getCourse().getCourse_id())
-				.orElseThrow(() -> new EntityNotFoundException("Course not found"));
-
-		assignment.setCourse(course);
+		// Update assignment
+		assignment.setDueDate(java.sql.Date.valueOf(updatedAssignment.dueDate()));
+		assignment.setName(updatedAssignment.assignmentName());
 
 		assignmentRepository.save(assignment);
-
-		return ResponseEntity.ok("Assignment modified successfully");
 	}
 
 	@DeleteMapping("/assignment/{assignmentId}")
-	public ResponseEntity<String> deleteAssignment(@PathVariable int assignmentId) {
-		Assignment assignment = assignmentRepository.findById(assignmentId)
-				.orElseThrow(() -> new EntityNotFoundException("Assignment not found"));
+	public void deleteAssignment(@PathVariable int assignmentId) {
+		// Check if the assignment exists
+		Assignment assignment = assignmentRepository.findById(assignmentId).orElse(null);
+		if (assignment == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Assignment not found");
+		}
 
+		// Delete assignment
 		assignmentRepository.delete(assignment);
-
-		return ResponseEntity.ok("Assignment deleted successfully");
 	}
 
 	// TODO create CRUD methods for Assignment
--- cst438_gradebook/src/test/java/com/cst438/JunitTestAssignment.java
+++ cst438_gradebook/src/test/java/com/cst438/JunitTestAssignment.java
@@ -1,10 +1,8 @@
 package com.cst438;
 
-import static org.hamcrest.Matchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.cst438.domain.Course;
+import com.cst438.domain.AssignmentDTO;
+import com.cst438.domain.AssignmentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,13 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.cst438.domain.Assignment;
-import com.cst438.domain.AssignmentRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.sql.Date;
-import java.time.LocalDate;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -31,34 +25,45 @@ public class JunitTestAssignment {
     @Autowired
     private AssignmentRepository assignmentRepository;
 
+    @Test
+    void testGetAllAssignment() throws Exception {
+        // Get all assignments - assert status 200
+        mvc.perform(MockMvcRequestBuilders.get("/assignment"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse();
+    }
 
     @Test
     void testAddAssignment() throws Exception {
-        Course testCourse = new Course();
-        testCourse.setTitle("Test Course");
-        testCourse.setCourse_id(1);
-        testCourse.setYear(2023);
-        testCourse.setInstructor("John Doe");
-        testCourse.setSemester("Fall");
-
-        // Create a sample AssignmentDTO for testing
-        Assignment assignment = new Assignment();
-        assignment.setName("Sample Assignment");
-        assignment.setDueDate(Date.valueOf("2023-09-30"));
-        assignment.setCourse(testCourse);
-
-
-//        String assignmentJson = asJsonString(assignment);
-//
-//        // Perform an HTTP POST request to add the assignment
-//        MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
-//                        .post("/assignment")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(assignmentJson))
-//                .andExpect(status().isOk())
-//                .andReturn().getResponse();
+        // Add assignment Testname - assert status 200
+        AssignmentDTO adto = new AssignmentDTO(1, "Testname", "2023-09-09", null, 31045);
+        mvc.perform(MockMvcRequestBuilders.post("/assignment")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(adto))
+                .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk());
     }
 
+    @Test
+    void testModifyAssignment() throws Exception {
+        // Modify assignment - assert status 200
+        AssignmentDTO updatedAssignment = new AssignmentDTO(3, "UpdatedName", "2023-09-09", null, 31046);
+        mvc.perform(MockMvcRequestBuilders.put("/assignment/{assignmentId}", 3)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(updatedAssignment))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testDeleteAssignment() throws Exception {
+        // Delete assignment - assert status 200
+        mvc.perform(MockMvcRequestBuilders.delete("/assignment/{assignmentId}", 1))
+                .andExpect(status().isOk());
+        // Delete a second time - assert status 404
+        mvc.perform(MockMvcRequestBuilders.delete("/assignment/{assignmentId}", 1))
+                .andExpect(status().isNotFound());
+    }
 
     private static String asJsonString(final Object obj) {
         try {
